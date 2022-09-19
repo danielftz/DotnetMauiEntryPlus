ae3e4d2d4b61bbd01476d51c87f07c79b3d49304
Daniel <daniel.fang@bintec.ca>
2022-09-18 23:16:36 -0700

    nullable fix

--- EntryPlus/EntryPlus/Platforms/Android/AndroidHideKeyboardOnTouchListener.cs
+++ EntryPlus/EntryPlus/Platforms/Android/AndroidHideKeyboardOnTouchListener.cs
@@ -11,6 +11,7 @@ namespace EntryPlus.Platforms.Android
     public class AndroidHideKeyboardOnTouchListener : Java.Lang.Object, IOnTouchListener
     {
         InputMethodManager? _imm;
+        WindowInsets? _rootWindowInsets;
         private readonly bool _isPartOfEntryPlus;
         public AndroidHideKeyboardOnTouchListener(bool isPartOfEntryPlus)
         {
@@ -20,24 +21,26 @@ namespace EntryPlus.Platforms.Android
 
         bool IOnTouchListener.OnTouch(AView? v, MotionEvent? e)
         {
-
             _imm ??= Platform.CurrentActivity?.GetSystemService(Context.InputMethodService) as InputMethodManager;
-            bool isKeyboardShown = WindowInsetsCompat.ToWindowInsetsCompat(Platform.CurrentActivity.Window.DecorView.RootWindowInsets).IsVisible(WindowInsetsCompat.Type.Ime());
-            if (_isPartOfEntryPlus is true && isKeyboardShown is true)
+            _rootWindowInsets ??= Platform.CurrentActivity?.Window?.DecorView.RootWindowInsets;
+            if (_rootWindowInsets is not null && _imm is not null)
             {
-                return true;
-            }
-            else if (isKeyboardShown is true)
-            {
-                AView? view = Platform.CurrentActivity?.CurrentFocus;
-                if (view is not null)
+                bool isKeyboardShown = WindowInsetsCompat.ToWindowInsetsCompat(_rootWindowInsets).IsVisible(WindowInsetsCompat.Type.Ime());
+                if (_isPartOfEntryPlus is true && isKeyboardShown is true)
                 {
-                    _imm.HideSoftInputFromWindow(view.WindowToken, HideSoftInputFlags.None);
                     return true;
+                }
+                else if (isKeyboardShown is true)
+                {
+                    AView? view = Platform.CurrentActivity?.CurrentFocus;
+                    if (view is not null)
+                    {
+                        _imm.HideSoftInputFromWindow(view.WindowToken, HideSoftInputFlags.None);
+                        return true;
+                    }
                 }
             }
             return false;
-           
         }
     }
 }
